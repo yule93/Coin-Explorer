68d1dd5ebffe6494a4b171f3e99dc99bba6fba58
여율 <dbfm2222@gmail.com>
2021-02-04 01:31:39 +0900

    ㅐㅎㅆ당 ㅠ ㅠㅠ

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,3 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default () => <header style="background-color: skyblue;">Coin Expoler</header>;
+export default () => {
+  return (
+    <header>
+      <ul>
+        <li>
+          <Link to="/prices">Prices</Link>
+        </li>
+        <li>
+          <Link to="/exchanges">Exchanges</Link>
+        </li>
+        <li>
+          <Link to="/coins">Coins</Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
 import Coins from "./Screens/Coins";
 import Prices from "./Screens/Prices";
 import Exchanges from "./Screens/Exchanges";
+import Header from "./Components/Header";
 
 export default () => {
-  <Router>
-  <>
-    <Route path="/prices" component={Prices} />
-    <Route path="/exchanges" component={Exchanges} />
-    <Route path="/coins" component={Coins} />
-  </>
-  </Router>
+  return (
+    <Router>
+      <Switch>
+        <Header />
+        <Route path="/prices" exact component={Prices} />
+        <Route path="/exchanges" exact component={Exchanges} />
+        <Route path="/coins" exact component={Coins} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </Router>
+  );
 };
